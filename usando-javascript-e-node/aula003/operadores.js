f9b7b026b8fc232d70f73d9b78d7b3ff5ba33ec1
Anne Bellandi <anne.bellandi@gmail.com>
2024-08-08 14:46:45 -0300

    pratica

---
+++ usando-javascript-e-node/aula003/operadores.js
@@ -0,0 +1,34 @@
+var n = 3;
+//n = n - 10;
+n *= 10;
+
+console.log(n);
+
+var x = 10;
+x--
+
+a = 20;
+b = 60;
+c = 60;
+
+console.log(x);
+
+console.log(n < x || b != c);
+
+//verificar se a é maior que b e b é divisível por 2
+
+console.log(a < b && b % 2 === 0);
+
+//verificar se b é maior igual a ou b divido por 2 o resultado é 2
+
+console.log(b >= a || b / 2 === 2);
+
+//verificar se a é maior que b e b maior igual a 5
+
+console.log(a > b && b >= 5);
+
+//verificar se b é par ou ímpar usando ternário
+resultado = b % 2 == 0 ? "par" : "impar"
+
+
+
---
+++ usando-javascript-e-node/aula003/prática.js
@@ -0,0 +1,18 @@
+var experienciaAnos = 5;
+var nivelEducacao = "superior";
+var possuiCertificado = true;
+
+// Critérios de elegibilidade
+var idadeMinima = 18;
+var experienciaMinimaAnos = 3;
+var nivelEducacaoMinimo = "superior";
+var certificadoObrigatorio = true;
+
+// Verificação de elegibilidade
+var elegivel =
+  experienciaAnos >= experienciaMinimaAnos &&
+  (nivelEducacao === nivelEducacaoMinimo || possuiCertificado) &&
+  (!certificadoObrigatorio || possuiCertificado);
+
+// Saída do resultado
+console.log("Elegível para a vaga? " + elegivel);
