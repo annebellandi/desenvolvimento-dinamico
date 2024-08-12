fa790afb64808bdb94fa8bf7a85d108b53f5e7d5
Anne Bellandi <anne.bellandi@gmail.com>
2024-08-12 18:18:26 -0300

    update

---
+++ usando-javascript-e-node/aula005/forEach.js
@@ -0,0 +1,8 @@
+var a = [10, 20, 30, 40, 50, 60];
+var tot = 0; 
+
+a.forEach(valor => {
+    tot += valor;
+});
+
+console.log(tot);
---
+++ usando-javascript-e-node/aula005/map.js
@@ -0,0 +1,16 @@
+var numeros = [1,2,3,4,5,6,7,8,9,10];
+
+var nums = numeros.map(function(valor){
+    return valor * 2;
+})
+console.log(nums);
+
+var funcionarios = [
+    {nome: "luis", idade:62},
+    {nome: "davi", idade:22},
+    {nome: "artur", idade:18},
+    {nome: "lucas", idade:40},
+]
+
+nomes = funcionarios.map(pessoa => pessoa.nome);
+console.log(nomes);
